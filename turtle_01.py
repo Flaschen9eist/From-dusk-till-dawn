bc6d3671e87a14205c7d44a1635d6e12ef43c3ea
Michael <flaschen9eist@gmail.com>
2021-09-18 19:54:56 +0200

    Turtle ausprobieren

---
+++ turtle_01.py
@@ -0,0 +1,22 @@
+import turtle
+
+#turtle.write('Turtle ausprobieren!', font=('Arial', 40, 'normal'))
+
+turtle.width(8)
+
+turtle.color('blue')
+turtle.circle(100)
+
+turtle.up()
+turtle.color('orange')
+turtle.forward(150)
+turtle.down()
+turtle.circle(100)
+
+turtle.up()
+turtle.color('green')
+turtle.backward(300)
+turtle.down()
+turtle.circle(100)
+
+turtle.done()
